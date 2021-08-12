3a95f85347bf372e88561a5527913e8f597b5bc3
dogukanteber <dogukanteber1@hotmail.com>
2021-08-12 22:16:59 +0300

    Change allowed hosts.

--- cms/settings.py
+++ cms/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@*!mixmzxaoswk0o=cr76q6594ia_oh&z1!zdoks#@ey**br(1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['awesomecms.herokuapp.com']
 
 
 # Application definition
